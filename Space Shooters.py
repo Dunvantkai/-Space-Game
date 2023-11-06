bc9e012fdb0bc2dec1f7ac32de42e664e8053ec0
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2023-11-07 03:55:47 +1300

    update

--- Space Shooters.py
+++ Space Shooters.py
@@ -3,13 +3,13 @@ import os
 import random
 pygame.init()
 os.system('cls')
-print("t1")
+print("Kai's Space Shooter program")
 # render screen
 screen = pygame.display.set_mode((800, 600))
 
 #Titile
-pygame.display.set_caption("T1")
-icon = pygame.image.load('world.png')
+pygame.display.set_caption("Kai's Space Shooters")
+icon = pygame.image.load('backdrop.png')
 pygame.display.set_icon(icon)
 
 #background
@@ -20,6 +20,7 @@ playerIco = pygame.image.load('ship.png')
 playerX = 370
 playerY = 500
 playerX_change = 0
+score = 0
 
 #enmy
 enmyIco = pygame.image.load('enmy.png')
@@ -34,35 +35,29 @@ bulletY = 500
 bulletX_chnage = 0
 bulletY_change = 0.2
 bullet_state = "ready"
-
-score = 0
-
+#player render
 def player(x, y):
     screen.blit(playerIco, (x, y))
-
+#enmy render
 def enmy(x, y):
     screen.blit(enmyIco, (x, y))
-
+#bullet render
 def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(bulletIco,(x + 19.4, y))
-
+#hit box check
 def Hitbox_math():
     global hit_enmy
     bulletX = fix_bullet_location
-    enmyXB = enmyX + 64
-    bulletXB = bulletX + 64
-    enmyYB = enmyY + 64
-    bulletYB = bulletY + 64
+    enmyXB = enmyX + 60
+    bulletXB = bulletX + 60
+    enmyYB = enmyY + 60
+    bulletYB = bulletY + 60
     hit_enmy = False
     if bulletX >= enmyX and bulletX <= enmyXB or bulletXB <= enmyXB and bulletXB >= enmyX:
         if bulletY >= enmyY and bulletY <= enmyYB or bulletYB <= enmyYB and bulletYB >= enmyY:
             hit_enmy = True
-
-
-
-
 # Tick game loop
 isRunning = True
 while isRunning:
@@ -93,7 +88,7 @@ while isRunning:
             elif event.key == pygame.K_RIGHT:
                 print("KeyReg(Right Lifted)")
                 playerX_change = 0.0
-
+    #player walls
     playerX += playerX_change
     if playerX > 738:
         playerX = 736
@@ -101,7 +96,7 @@ while isRunning:
         playerX = 0
 
     enmyX += enmyX_change
-
+    #enmy edge of screen bons
     if enmyX > 738:
         enmyX_change = -0.1
         enmyY += enmyY_change
@@ -112,15 +107,15 @@ while isRunning:
     if bulletY <= -40:
         bullet_state = "ready"
         bulletY = 500
-        
+      #bullet moves up  
     if bullet_state == "fire":
         fire_bullet(fix_bullet_location, bulletY)
         bulletY -= bulletY_change
         Hitbox_math()
-
+        # kill check
         if hit_enmy == True:
             score = score + 1
-            print(score)
+            print("Your Score is:", score)
             bullet_state = "ready"
             bulletY = -40
             enmyX = random.randint(0, 736)
